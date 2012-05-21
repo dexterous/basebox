1aa15f35b1ea90aa32cd581866ce7e3ff2c4857b
Stephen Delano <stephen@opscode.com>
2012-05-21 15:16:43 -0700

    use the i386 iso

--- centos-5.5-i386/definition.rb
+++ centos-5.5-i386/definition.rb
@@ -1,10 +1,11 @@
 require File.dirname(__FILE__) + "/../.centos/session.rb"
 
-iso = "CentOS-5.5-x86_64-bin-DVD-1of2.iso"
+iso = "CentOS-5.5-i386-bin-DVD.iso"
 
 session =
-  CENTOS_SESSION.merge({ :iso_file => iso,
-                         :iso_md5 => "ac177a5476e3b255b89b659e5b10ba03",
-                         :iso_src => "http://vault.centos.org/5.5/isos/x86_64/#{iso}" })
+  CENTOS_SESSION.merge({ :os_type_id => 'RedHat',
+                         :iso_file => iso,
+                         :iso_md5 => "75c92246479df172de41b14c9b966344",
+                         :iso_src => "http://vault.centos.org/5.5/isos/i386/#{iso}" }) # not really there, must use torrent
 
 Veewee::Session.declare session
